8ea8f2f2a1decc006640981524bb69a419be06fc
Aadhavan-Ramkumar <74062499+Aadhavan-Ramkumar@users.noreply.github.com>
2022-02-09 14:18:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-var MenuList;
-MenuList = ["Chicken Tandoori Pizza", "Veg Supreme Pizza", "Paneer Tikka Pizza", "Deluxe Veggie Pizza", "Veg Extravaganza Pizza"];
+MenuList = ["Chicken Tandoori Pizza",
+    "Veg Supreme Pizza",
+    "Paneer Tikka Pizza",
+    "Deluxe Veggie Pizza",
+    "Veg Extravaganza Pizza"
+];
 
-function getmenu() {
-    var htmldata;
-    htmldata = "<ol class>='menulist'>";
+function GetMenu() {
+    let HTMLData = "<ol class='MenuList'>";
     MenuList.sort();
     for (var i = 0; i < MenuList.length; i++) {
-        htmldata = htmldata = '<li>' + MenuList[i] + '</li>';
+        HTMLData += '<li>' + MenuList[i] + '</li>';
     }
-    htmldata = htmldata + "</ol>";
-    document.getElementById("display_menu").innerHTML = htmldata;
+    HTMLData += "</ol>";
+    document.getElementById("DisplayMenu").innerHTML = HTMLData;
 }
 
-function add_item() {
-    var htmldata;
-    var item = document.getElementById("add_item").value;
-    MenuList.push(item);
+function AddItem() {
+    let HTMLData;
+    let Item = document.getElementById("AddItem").value;
+    MenuList.push(Item);
     MenuList.sort();
-    htmldata = "<section class='cards'>";
+    HTMLData = "<section class='Cards'>"
     for (var i = 0; i < MenuList.length; i++) {
-        htmldata = htmldata + '<div class="card">' + '<img src="Pizza.png">' + MenuList[i] + '</div>'
+        HTMLData += '<div class="Card">' + '<img src="Pizza 2.png">' + MenuList[i] + '</div>';
     }
-    htmldata = htmldata + "</section>";
-    document.getElementById("display_addedmenu").innerHTML = htmldata;
+    HTMLData += "</section>";
+    document.getElementById("DisplayAddedMenu").innerHTML = HTMLData;
 }
